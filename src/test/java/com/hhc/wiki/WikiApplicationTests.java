380b98e9bd1be6060d7baa4c4c36fe0acab80723
HHC <HHC20000201@163.com>
2023-06-13 11:09:38 +0800

    2-2 init

---
+++ src/test/java/com/hhc/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhc.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
