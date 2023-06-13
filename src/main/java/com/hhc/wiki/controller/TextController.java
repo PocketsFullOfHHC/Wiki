60230839452fe5d0c4de4e1519a4b3b36cb81558
HHC <HHC20000201@163.com>
2023-06-13 20:34:35 +0800

    2-7 增加hello接口测试text.http

--- src/main/java/com/hhc/wiki/controller/TextController.java
+++ src/main/java/com/hhc/wiki/controller/TextController.java
@@ -1,6 +1,7 @@
 package com.hhc.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 返回字符串
@@ -18,5 +19,10 @@ public class TextController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post," + name;
     }
 }
