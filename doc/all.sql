3b1489c60a17a4dd013563c6d816dd4851f33d66
HHC <HHC20000201@163.com>
2023-06-14 12:09:44 +0800

    3-3 增加all.sql脚本

---
+++ doc/all.sql
@@ -0,0 +1,11 @@
+# 如果存在test表就将其删去
+drop table if exists `test`;
+# 注意表或者字段的名字要用反引号
+create table `test` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment = '测试';
+
+select * from test;
