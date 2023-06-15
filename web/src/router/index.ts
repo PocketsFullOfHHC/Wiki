230a6942d0dc67d71c9fbdb3dacc196d20310bc9
HHC <HHC20000201@163.com>
2023-06-15 21:05:34 +0800

    4-3 使用Vue CLI生成web项目

---
+++ web/src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded(懒加载：表示该组件只有当访问该页面时或者空闲时间才会加载，并不会在一次性加载完，可以减小初始包的大小) when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
