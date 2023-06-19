c35fcd37cbc8bf5591e56a47d029dbd6965a5a04
HHC <HHC20000201@163.com>
2023-06-19 10:52:44 +0800

    5-3 使用ref和reactive实现数据绑定

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -38,24 +38,47 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>
+        <!-- 直接打印会变成一个json字符串(由数组包含)，用pre标签，表示预定义格式文本。在该元素中的文本通常按照原文件中的编排，以等宽字体的形式展现出来，文本中的空白符（比如空格和换行符）都会显示出来 -->
+{{ebooks}}
+{{ebooks2}}
+      </pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'HomeView',
   // vue3新增的初始化方法，是这个组件加载完成后初始会去执行的方法
+  // vue3将vue2中的data，method，以及mounted等生命周期函数打包成setup
   setup(){
     console.log("setup");
-    // 返回值为响应内容，用response接收
-    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
-      console.log(response);
-    })
+    // vue3新增ref为定义响应式数据，即在js中动态修改这里面的值，需要实时反馈到界面上
+    const ebooks = ref();
+    // reactive中需要放置一个对象，在该对象中添加属性books属性来放response返回值并将该对象赋值给ebooks
+    const ebooks1 = reactive({books:[]});
+    // 一般把界面的的初始化逻辑都写到onMounted中，不建议直接写到setup()中
+    onMounted(() => {
+      console.log("onMounted");
+      // 返回值为响应内容，用response接收
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        // ref的对应赋值方法
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(response);
+      })
+    });
+    return{
+      // 前端需要拿到这个数据，因此要在setup后返回这个值，类似data
+      ebooks,
+      // 将其转化为响应式数据
+      ebooks2: toRef(ebooks1, "books")
+    }
   }
 });
 </script>
