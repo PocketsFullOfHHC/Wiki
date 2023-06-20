b07ff1176655b0ca6900a10cf9a2c90b3b1cd61f
HHC <HHC20000201@163.com>
2023-06-20 11:40:40 +0800

    5-5 修改axios的baseUrl全局读取配置文件

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,6 +6,9 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 // 引入组件库的图标库
 import * as Icons from '@ant-design/icons-vue';
+// 全局配置axios请求域(域：即请求地址的前半段：http://127.0.0.1:8880)
+import axios from 'axios';
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
