cb76b21b8920716572a6cac022a98717d7911c00
HHC <HHC20000201@163.com>
2023-06-21 11:14:27 +0800

    6-2 修改组件名字为小写字母开头，并修改eslint的检查限制

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -17,5 +17,7 @@ module.exports = {
     'vue/no-unused-components': 'off',
     // 取消typescript使用any的警告
     '@typescript-eslint/no-explicit-any': 0,
+    // 取消组件名称驼峰命名法的报错提示
+    "vue/multi-word-component-names": "off",
   }
 }
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded(懒加载：表示该组件只有当访问该页面时或者空闲时间才会加载，并不会在一次性加载完，可以减小初始包的大小) when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: About
   }
 ]
 
