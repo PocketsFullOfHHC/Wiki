5c513cf1c8e100865b8c6ab80dd86148be968862
HHC <HHC20000201@163.com>
2023-07-02 17:17:21 +0800

    8-7集成富文本框插件wangEditor@4.6.3，在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -82,6 +82,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -92,6 +95,8 @@
   import {Tool} from "@/util/tool";
   import {useRoute} from "vue-router";
   import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+  import E from 'wangeditor';
+  import i18next from "i18next";
 
   export default defineComponent({
     name: 'AdminDoc',
@@ -177,6 +182,8 @@
       const doc = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
+      const editor = new E('#content');
+
       // 点击OK后的逻辑
       const handleModalOk = () => {
         modalLoading.value = true;
@@ -274,6 +281,11 @@
 
         // 在选择树前面添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
+
+        setTimeout(function () {
+          editor.i18next = i18next;
+          editor.create();
+        }, 100);
       };
 
       /**
@@ -290,6 +302,11 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
+        setTimeout(function () {
+          // 下载i18next的包并在这里配置解决富文本框第二次初始化报错的
+          editor.i18next = i18next;
+          editor.create();
+        }, 100);
       };
 
       /**
