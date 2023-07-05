62a782c38786a71097873b55a0c50b664676cdca
HHC <HHC20000201@163.com>
2023-07-05 22:37:01 +0800

    9-14 用户密码初始化：test/test

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -76,7 +76,7 @@
             // 用来登录
             const loginUser = ref({
                 loginName: "test",
-                password: "test123"
+                password: "test"
             });
             const loginModalVisible = ref(false);
             const loginModalLoading = ref(false);
