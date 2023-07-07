80302f1184eacdc75ca789456671fc3d0f1b0f23
HHC <HHC20000201@163.com>
2023-07-07 21:24:15 +0800

    10-10 将业务线程日志流水号传递到异步线程，方便生产运维查问题

--- src/main/java/com/hhc/wiki/service/WsService.java
+++ src/main/java/com/hhc/wiki/service/WsService.java
@@ -1,6 +1,7 @@
 package com.hhc.wiki.service;
 
 import com.hhc.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,8 @@ public class WsService {
 
     // 异步化的方法一定要单独写到一个类中
     @Async
-    public void sendInfo(String message) {
+    public void sendInfo(String message, String logId) {
+        MDC.put("LOG_ID", logId);
         webSocketServer.sendInfo(message);
     }
 }
