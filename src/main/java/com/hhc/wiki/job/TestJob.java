f51e4b6cb52a453adbadf19590a869ef1ef7871b
HHC <HHC20000201@163.com>
2023-07-07 11:28:42 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL

--- src/main/java/com/hhc/wiki/job/TestJob.java
+++ src/main/java/com/hhc/wiki/job/TestJob.java
@@ -1,42 +1,42 @@
-// 所有的定时器都只有一个线程，如果第一个定时器暂停一段时间，第二个定时器必须等暂停结束后才能继续自己的定时器
-package com.hhc.wiki.job;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-@Component
-public class TestJob {
-
-    private static final Logger LOG = LoggerFactory.getLogger(TestJob.class);
-
-    /**
-     * 固定时间间隔，fixedRate单位毫秒
-     */
-    @Scheduled(fixedRate = 1000)
-    public void simple() throws InterruptedException {
-        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
-        String dateString = formatter.format(new Date());
-        Thread.sleep(2000);
-        LOG.info("每隔1秒钟执行一次： {}", dateString);
-    }
-
-    /**
-     * 自定义cron表达式跑批
-     * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
-     */
-    @Scheduled(cron = "*/1 * * * * ?")
-    public void cron() throws InterruptedException {
-        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss SSS");
-        String dateString = formatter.format(new Date());
-        Thread.sleep(1500);
-        LOG.info("每隔1秒钟执行一次： {}", dateString);
-    }
-
-}
-
-
+//// 所有的定时器都只有一个线程，如果第一个定时器暂停一段时间，第二个定时器必须等暂停结束后才能继续自己的定时器
+//package com.hhc.wiki.job;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//@Component
+//public class TestJob {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(TestJob.class);
+//
+//    /**
+//     * 固定时间间隔，fixedRate单位毫秒
+//     */
+//    @Scheduled(fixedRate = 1000)
+//    public void simple() throws InterruptedException {
+//        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
+//        String dateString = formatter.format(new Date());
+//        Thread.sleep(2000);
+//        LOG.info("每隔1秒钟执行一次： {}", dateString);
+//    }
+//
+//    /**
+//     * 自定义cron表达式跑批
+//     * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
+//     */
+//    @Scheduled(cron = "*/1 * * * * ?")
+//    public void cron() throws InterruptedException {
+//        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss SSS");
+//        String dateString = formatter.format(new Date());
+//        Thread.sleep(1500);
+//        LOG.info("每隔1秒钟执行一次： {}", dateString);
+//    }
+//
+//}
+//
+//
--- src/main/java/com/hhc/wiki/service/DocService.java
+++ src/main/java/com/hhc/wiki/service/DocService.java
@@ -172,4 +172,8 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+
+    public void updateEbookInfo() {
+        docMapperCust.updateEbookInfo();
+    }
 }
