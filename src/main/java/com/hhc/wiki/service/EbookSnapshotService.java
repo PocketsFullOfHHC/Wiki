f5393b554fb0068efc18f8ef68115c594218a0f2
HHC <HHC20000201@163.com>
2023-07-24 12:26:37 +0800

    11-5 增加定时任务，定时收集数据 要在xml里一次执行多个SQL的话，需要为数据库连接增加参数：allowMultiQueries=true

---
+++ src/main/java/com/hhc/wiki/service/EbookSnapshotService.java
@@ -0,0 +1,18 @@
+package com.hhc.wiki.service;
+
+import com.hhc.wiki.mapper.EbookSnapshotMapperCust;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapperCust ebookSnapshotMapperCust;
+
+    public void genSnapshot() {
+        ebookSnapshotMapperCust.genSnapshot();
+    }
+
+}
