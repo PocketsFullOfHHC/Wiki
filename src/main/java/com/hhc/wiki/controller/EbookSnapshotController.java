ba92764f8cdbb58505eedb91b0440cfb8b060ba6
HHC <HHC20000201@163.com>
2023-07-24 12:39:05 +0800

    11-6 后端增加获取统计数值接口

---
+++ src/main/java/com/hhc/wiki/controller/EbookSnapshotController.java
@@ -0,0 +1,28 @@
+package com.hhc.wiki.controller;
+
+import com.hhc.wiki.resp.CommonResp;
+import com.hhc.wiki.resp.StatisticResp;
+import com.hhc.wiki.service.EbookSnapshotService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+}
--- src/main/java/com/hhc/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/hhc/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,5 +1,12 @@
 package com.hhc.wiki.mapper;
 
+import com.hhc.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
+
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
